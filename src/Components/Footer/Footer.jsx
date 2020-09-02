ed12d1eb1787e1482b7e478a3c944f4ac4316759
Francesco Taddia <fctaddia@gmail.com>
2020-09-02 19:29:56 +0200

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  const onMouseOut = (e) => {
+    e.target.style.color = "#cccccc";
+  };
+
+  const SocialWidget = [
+    {
+      SocialMediaName: "GitHub",
+      ProfileLink: "https://github.com/fctaddia",
+      fontAwesomeIconName: "fab fa-github",
+      OnMouseOverColor: "#211F1F",
+    },
+    {
+      SocialMediaName: "StackOverFlow",
+      ProfileLink:
+        "https://stackoverflow.com/users/11605000/francesco-taddia",
+      fontAwesomeIconName: "fab fa-stack-overflow",
+      OnMouseOverColor: "#f48024",
+    },
+    {
+      SocialMediaName: "Gmail",
+      ProfileLink: "mailto:fctaddia@gmail.com",
+      fontAwesomeIconName: "far fa-envelope",
+      OnMouseOverColor: "#D44638",
+      _comment: "OnMouseOverColor for yahoo mail: #720e9e",
+    }
+  ];
+
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 30,
+            marginTop: "40px",
+            textAlign: "center",
+          }}
+        >
+          Created with care by{" "}
+          <a
+            href="https://github.com/fctaddia"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Francesco Taddia
+          </a>
+        </h6>
+      </div>
+      <div className={styles.container}>
+        <br />
+        <ul className="list" style={{ listStyleType: "none" }}>
+          {SocialWidget.map((data, key) => {
+            return (
+              <li
+                key={key}
+                style={{ display: "inline-block", marginRight: "17px" }}
+              >
+                <a
+                  onMouseOver={(e) => {
+                    e.target.style.color = `${data.OnMouseOverColor}`;
+                  }}
+                  onMouseOut={onMouseOut}
+                  href={data.ProfileLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "#cccccc", fontSize: "40px" }}
+                >
+                  <i className={`${data.fontAwesomeIconName}`}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className={styles.container}>
+        <h6
+          style={{
+            display: "block",
+            marginBottom: "0px",
+            fontSize: 16,
+            marginTop: "20px",
+            textAlign: "center",
+          }}
+        >
+          Data provided by{" "}
+          <a
+            href="https://github.com/mathdroid/covid-19-api"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "rgba(0,0,255,0.6)", textDecoration: "none" }}
+          >
+            Mathdroid
+          </a>
+        </h6>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
